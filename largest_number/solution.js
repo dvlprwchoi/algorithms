ce2b68140ab45e57e69adfa3edd8ddf1f01d81ec
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-06-07 09:48:02 -0500

    Largest number

--- largest_number/solution.js
+++ largest_number/solution.js
@@ -1,1 +1,14 @@
+const largestNumber = (num) => {
+    if (num === 0) {
+        return 0;
+    }
 
+    if(num > 0) {
+        let largestNumber = "9";
+        return parseInt(largestNumber.repeat(num))
+    }
+}
+
+console.log(largestNumber(0));
+console.log(largestNumber(1));
+console.log(largestNumber(7));
