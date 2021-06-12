9d6d32266fa2c1f269cbc7862fb3b57a518df296
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-06-12 00:50:54 -0500

    Constraints

--- solve_me_first/solution.js
+++ solve_me_first/solution.js
@@ -1,4 +1,8 @@
 const solveMeFirst = (a, b) => {
-    return a + b;
+    if (a <= 1000 && b <= 1000) {
+        return a + b;
+    }
 }
 console.log(solveMeFirst(2,3));
+console.log(solveMeFirst(1001, 900));
+console.log(solveMeFirst(1000, 999));
